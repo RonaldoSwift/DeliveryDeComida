0152f8efcf82a38437fd8b025b13068f7549e616
Ronaldo Vargas <ronaldovh99@gmail.com>
2021-12-23 13:28:53 -0500

    Resolviendo algunas observaciones

--- DeliveryDeComida/CartView.swift
+++ DeliveryDeComida/CartView.swift
@@ -14,55 +14,7 @@ struct CartView: View {
                 .foregroundColor(Color("ColorDeTituloDeMenu"))
                 .font(.title)
             
-            ScrollView(.horizontal){
-                HStack{
-                    Button {
-                    } label: {
-                        Image(systemName: "arrow.left.arrow.right")
-                            .foregroundColor(Color("ColorDeLinkViewEnCartView"))
-                        Text("Filters")
-                            .foregroundColor(Color("ColorDeTituloDeMenu"))
-                    }
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(4)
-                    
-                    Button {
-                    } label: {
-                        Image(systemName: "paperplane")
-                            .foregroundColor(Color.white)
-                        Text("Nearby")
-                            .foregroundColor(Color.white)
-                    }
-                    .padding()
-                    .background(Color("ColorDeLinkViewEnCartView"))
-                    .cornerRadius(4)
-                    
-                    Button {
-                    } label: {
-                        Image(systemName: "star")
-                            .foregroundColor(Color("ColorDeLinkViewEnCartView"))
-                        Text("Above 4.5")
-                            .foregroundColor(Color("ColorDeTituloDeMenu"))
-                    }
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(4)
-                    
-                    Button {
-                    } label: {
-                        Image(systemName: "tag")
-                            .foregroundColor(Color("ColorDeLinkViewEnCartView"))
-                        Text("Check")
-                            .foregroundColor(Color("ColorDeTituloDeMenu"))
-                    }
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(4)
-                }
-                .padding()
-            }
-            
+            FiltrosView()
             VStack(alignment: .leading){
                 HStack(alignment: .center, spacing: 40){
                     VStack(alignment: .leading){
@@ -89,213 +41,77 @@ struct CartView: View {
                     .foregroundColor(Color("ColorDeNumeroAnulado"))
                 
                 ScrollView{
-                    VStack(alignment: .trailing){
-                        HStack{
-                            VStack{
-                                HStack(spacing: 20){
-                                    Image("Comida1")
-                                        .resizable()
-                                        .frame(width: 100, height: 120, alignment: .leading)
-                                        .cornerRadius(4)
-                                    VStack(alignment: .leading, spacing: 10){
-                                        Text("Fruit salad mix")
-                                            .foregroundColor(Color("ColorDeTituloDeMenu"))
-                                            .font(.title2)
-                                        HStack{
-                                            Text("18.500")
-                                                .foregroundColor(Color("ColorDeNumero"))
-                                            Text("24.500")
-                                                .foregroundColor(Color("ColorDeNumeroAnulado"))
-                                        }
-                                        HStack{
-                                            Text("%")
-                                                .padding(3)
-                                                .foregroundColor(Color.white)
-                                                .background(Color("ColorText"))
-                                                .cornerRadius(10)
-                                            Text("Free delivery")
-                                                .foregroundColor(Color("ColorDeNumero"))
-                                        }
-                                    }
-                                }
-                            }
-                            Image(systemName: "heart")
-                                .resizable()
-                                .frame(width: 25, height: 25, alignment: .center)
-                                .foregroundColor(Color("ColorDeLinkViewEnCartView"))
-                                .background(Color.white)
-                                .cornerRadius(5)
-                                .padding()
-                        }
-                        HStack{
-                            Image(systemName: "pencil")
-                                .resizable()
-                                .frame(width: 25, height: 25, alignment: .center)
-                                .foregroundColor(Color("ColorDeLinkViewEnCartView"))
-                                .padding()
-                            Button {
-                            } label: {
-                                Text("-")
-                                    .padding()
-                                    .foregroundColor(Color.white)
-                                    .background(Color("ColorDeMenos"))
-                                    .cornerRadius(4)
-                            }
-                            Text("1")
-                                .padding()
-                                .foregroundColor(Color("ColorDeTituloDeMenu"))
-                                .background(Color("ColorDeAumentoDeNumero"))
-                                .cornerRadius(4)
-                            Button {
-                            } label: {
-                                Text("+")
-                                    .padding()
-                                    .foregroundColor(Color.white)
-                                    .background(Color("ColorDeLinkViewEnCartView"))
-                                    .cornerRadius(4)
-                            }
-                        }
-                    }
-                    
-                    VStack(alignment: .trailing){
-                        HStack{
-                            VStack{
-                                HStack(spacing: 20){
-                                    Image("Comida1")
-                                        .resizable()
-                                        .frame(width: 100, height: 120, alignment: .leading)
-                                        .cornerRadius(4)
-                                    VStack(alignment: .leading, spacing: 10){
-                                        Text("Fruit salad mix")
-                                            .foregroundColor(Color("ColorDeTituloDeMenu"))
-                                            .font(.title2)
-                                        HStack{
-                                            Text("18.500")
-                                                .foregroundColor(Color("ColorDeNumero"))
-                                            Text("24.500")
-                                                .foregroundColor(Color("ColorDeNumeroAnulado"))
-                                        }
-                                        HStack{
-                                            Text("%")
-                                                .padding(3)
-                                                .foregroundColor(Color.white)
-                                                .background(Color("ColorText"))
-                                                .cornerRadius(10)
-                                            Text("Free delivery")
-                                                .foregroundColor(Color("ColorDeNumero"))
+                    ForEach(0..<2){_ in
+                        VStack(alignment: .trailing){
+                            HStack{
+                                VStack{
+                                    HStack(spacing: 20){
+                                        Image("Comida1")
+                                            .resizable()
+                                            .frame(width: 100, height: 120, alignment: .leading)
+                                            .cornerRadius(4)
+                                        VStack(alignment: .leading, spacing: 10){
+                                            Text("Fruit salad mix")
+                                                .foregroundColor(Color("ColorDeTituloDeMenu"))
+                                                .font(.title2)
+                                            HStack{
+                                                Text("18.500")
+                                                    .foregroundColor(Color("ColorDeNumero"))
+                                                Text("24.500")
+                                                    .foregroundColor(Color("ColorDeNumeroAnulado"))
+                                            }
+                                            HStack{
+                                                Text("%")
+                                                    .padding(3)
+                                                    .foregroundColor(Color.white)
+                                                    .background(Color("ColorText"))
+                                                    .cornerRadius(10)
+                                                Text("Free delivery")
+                                                    .foregroundColor(Color("ColorDeNumero"))
+                                            }
                                         }
                                     }
                                 }
-                            }
-                            Image(systemName: "heart")
-                                .resizable()
-                                .frame(width: 25, height: 25, alignment: .center)
-                                .foregroundColor(Color("ColorDeLinkViewEnCartView"))
-                                .background(Color.white)
-                                .cornerRadius(5)
-                                .padding()
-                        }
-                        HStack{
-                            Image(systemName: "pencil")
-                                .resizable()
-                                .frame(width: 25, height: 25, alignment: .center)
-                                .foregroundColor(Color("ColorDeLinkViewEnCartView"))
-                                .padding()
-                            Button {
-                            } label: {
-                                Text("-")
+                                Image(systemName: "heart")
+                                    .resizable()
+                                    .frame(width: 25, height: 25, alignment: .center)
+                                    .foregroundColor(Color("ColorDeLinkViewEnCartView"))
+                                    .background(Color.white)
+                                    .cornerRadius(5)
                                     .padding()
-                                    .foregroundColor(Color.white)
-                                    .background(Color("ColorDeMenos"))
-                                    .cornerRadius(4)
                             }
-                            Text("1")
-                                .padding()
-                                .foregroundColor(Color("ColorDeTituloDeMenu"))
-                                .background(Color("ColorDeAumentoDeNumero"))
-                                .cornerRadius(4)
-                            Button {
-                            } label: {
-                                Text("+")
+                            HStack{
+                                Image(systemName: "pencil")
+                                    .resizable()
+                                    .frame(width: 25, height: 25, alignment: .center)
+                                    .foregroundColor(Color("ColorDeLinkViewEnCartView"))
                                     .padding()
-                                    .foregroundColor(Color.white)
-                                    .background(Color("ColorDeLinkViewEnCartView"))
-                                    .cornerRadius(4)
-                            }
-                        }
-                    }
-                    
-                    VStack(alignment: .trailing){
-                        HStack{
-                            VStack{
-                                HStack(spacing: 20){
-                                    Image("Comida1")
-                                        .resizable()
-                                        .frame(width: 100, height: 120, alignment: .leading)
+                                Button {
+                                } label: {
+                                    Text("-")
+                                        .padding()
+                                        .foregroundColor(Color.white)
+                                        .background(Color("ColorDeMenos"))
                                         .cornerRadius(4)
-                                    VStack(alignment: .leading, spacing: 10){
-                                        Text("Fruit salad mix")
-                                            .foregroundColor(Color("ColorDeTituloDeMenu"))
-                                            .font(.title2)
-                                        HStack{
-                                            Text("18.500")
-                                                .foregroundColor(Color("ColorDeNumero"))
-                                            Text("24.500")
-                                                .foregroundColor(Color("ColorDeNumeroAnulado"))
-                                        }
-                                        HStack{
-                                            Text("%")
-                                                .padding(3)
-                                                .foregroundColor(Color.white)
-                                                .background(Color("ColorText"))
-                                                .cornerRadius(10)
-                                            Text("Free delivery")
-                                                .foregroundColor(Color("ColorDeNumero"))
-                                        }
-                                    }
                                 }
-                            }
-                            Image(systemName: "heart")
-                                .resizable()
-                                .frame(width: 25, height: 25, alignment: .center)
-                                .foregroundColor(Color("ColorDeLinkViewEnCartView"))
-                                .background(Color.white)
-                                .cornerRadius(5)
-                                .padding()
-                        }
-                        HStack{
-                            Image(systemName: "pencil")
-                                .resizable()
-                                .frame(width: 25, height: 25, alignment: .center)
-                                .foregroundColor(Color("ColorDeLinkViewEnCartView"))
-                                .padding()
-                            Button {
-                            } label: {
-                                Text("-")
+                                Text("1")
                                     .padding()
-                                    .foregroundColor(Color.white)
-                                    .background(Color("ColorDeMenos"))
+                                    .foregroundColor(Color("ColorDeTituloDeMenu"))
+                                    .background(Color("ColorDeAumentoDeNumero"))
                                     .cornerRadius(4)
-                            }
-                            Text("1")
-                                .padding()
-                                .foregroundColor(Color("ColorDeTituloDeMenu"))
-                                .background(Color("ColorDeAumentoDeNumero"))
-                                .cornerRadius(4)
-                            Button {
-                            } label: {
-                                Text("+")
-                                    .padding()
-                                    .foregroundColor(Color.white)
-                                    .background(Color("ColorDeLinkViewEnCartView"))
-                                    .cornerRadius(4)
+                                Button {
+                                } label: {
+                                    Text("+")
+                                        .padding()
+                                        .foregroundColor(Color.white)
+                                        .background(Color("ColorDeLinkViewEnCartView"))
+                                        .cornerRadius(4)
+                                }
                             }
                         }
                     }
                 }
             }
-            
-            
             NavigationLink(destination: StoreView()) {
                 HStack(alignment: .center, spacing: 55){
                     Text("1 item")
